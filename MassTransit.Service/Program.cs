d3d8922d698e9f3b75defcad83ad1b8c14ad0f67
everton david <everton.097@gmail.com>
2022-08-20 12:37:05 -0300

    Add projeto service

---
+++ MassTransit.Service/Program.cs
@@ -0,0 +1,9 @@
+using MassTransit.Service;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
